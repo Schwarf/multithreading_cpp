9667ac9bab487aa4e12a2a1388b792895dc314e4
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-11 09:11:29 +0100

    Thread alarm finished.

--- posix_threads/alarm/thread_alarm.cpp
+++ posix_threads/alarm/thread_alarm.cpp
@@ -23,3 +23,33 @@ void *alarm_thread(void * arg)
 	return nullptr;
 }
 
+int main(int argc, char *argv[])
+{
+	int status;
+	char line[128];
+	AlarmTag * alarm;
+	pthread_t thread;
+	while(true)
+	{
+		printf("Alarm> ");
+		if( fgets(line, sizeof(line), stdin) == nullptr)
+			exit (0);
+		if(strlen(line) <= 1)
+			continue;
+		alarm = (AlarmTag*)malloc(sizeof (AlarmTag) );
+		if(!alarm)
+			errno_abort("Error in allocating alarm-memory");
+		if(sscanf (line, "%d %64[^\n]", &alarm->seconds, alarm->message) < 2)
+		{
+			fprintf(stderr, "Bad command \n");
+			free(alarm);
+		}
+		else
+		{
+			status = pthread_create(&thread, nullptr, alarm_thread, alarm);
+			if(status != 0)
+				err_abort(status, "Error in creating alarm thread");
+		}
+	}
+
+}
