f2e4d8663f165e45e68d2a58d7180dea6bbd3ece
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-11 08:10:55 +0100

    Add posix thread examples.

---
+++ posix_threads/alarm/classic_alarm.cpp
@@ -0,0 +1,31 @@
+//
+// Created by andreas on 09.02.23.
+//
+#include "errors.h"
+
+int main(int argc, char *argv[])
+{
+	int seconds;
+	char line[128];
+	char message[64];
+
+	while (true) {
+		printf("Alarm> ");
+		if (fgets(line, sizeof(line), stdin) == nullptr)
+			exit(0);
+		if (strlen(line) <= 1) continue;
+
+		/*
+		 * Parse input line into seconds (%d) and a message
+		 * (%64[^\n]), consisting of up to 64 characters
+		 * separated from the seconds by whitespace.
+		 */
+		if (sscanf(line, "%d %64[^\n]", &seconds, message) < 2) {
+			fprintf(stderr, "Bad command\n");
+		}
+		else {
+			sleep(seconds);
+			printf("(%d) %s\n", seconds, message);
+		}
+	}
+}
