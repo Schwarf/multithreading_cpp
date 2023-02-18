19ffcdf131f4fea11534b036fd4b3f61e7700398
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-18 19:35:53 +0100

    Finish mutex_alarm.

--- posix_threads/alarm/mutex_alarm.cpp
+++ posix_threads/alarm/mutex_alarm.cpp
@@ -36,7 +36,7 @@ void *alarm_thread(void *arguments)
 			{
 				alarm_list = alarm->link;
 				now = time(nullptr);
-				if(alarm->time < = now)
+				if(alarm->time <= now)
 					sleep_time = 0;
 				else
 					sleep_time = alarm->time - now;
@@ -58,4 +58,63 @@ void *alarm_thread(void *arguments)
 		}
 	}
 
+}
+
+int main(int number_of_arguments, char * argument_list[])
+{
+	int status;
+	char line[128];
+	alarm_tag *alarm, **last, *next;
+	pthread_t thread;
+	status = pthread_create(&thread, nullptr, alarm_thread, nullptr);
+	if(status != 0)
+		err_abort(status, "Error while creating alarm thread");
+	while(true) {
+		printf("Alarm >");
+		if (fgets(line, sizeof(line), stdin) == NULL)
+			exit(0);
+		if (strlen(line) <= 1)
+			continue;
+		alarm = (alarm_tag *)malloc(sizeof(alarm_tag));
+		if (alarm == nullptr)
+			errno_abort("Error in allocating alarm");
+		if (sscanf(line, "%d %64[^\n]", &alarm->seconds, alarm->message) < 2) {
+			fprintf(stderr, "Bad command \n");
+			free(alarm);
+		}
+		else
+		{
+			status = pthread_mutex_lock(&alarm_mutex);
+			if (status != 0)
+				err_abort(status, "Error while lock mutex to set alarm time");
+			alarm->time = time (nullptr) + alarm->seconds;
+			last = &alarm_list;
+			next = *last;
+			while(next != nullptr)
+			{
+				if(next->time >= alarm->time)
+				{
+					alarm->link = next;
+					*last = alarm;
+					break;
+				}
+				last = &next->link;
+				next = next->link;
+			}
+			if(next == nullptr)
+			{
+				*last = alarm;
+				alarm->link = nullptr;
+			}
+#ifdef DEBUG
+			printf("list: ");
+			for(next = alarm_list; next != nullptr; next = next->link)
+				printf("%d(%d)[\"%s]\"]) ", next->time, next->time - time(nullptr), next->message);
+			printf("]\n");
+#endif
+			status = pthread_mutex_unlock (&alarm_mutex);
+			if (status != 0)
+				err_abort (status, "Error while unlock mutex to set alarm time");
+		}
+	}
 }
