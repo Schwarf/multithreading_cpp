c2ae75a197d26a0ab7ee90c97a5e0d66a0d2d232
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-18 19:08:57 +0100

    Add thread-function for alarm.

--- posix_threads/alarm/mutex_alarm.cpp
+++ posix_threads/alarm/mutex_alarm.cpp
@@ -31,6 +31,31 @@ void *alarm_thread(void *arguments)
 		alarm = alarm_list;
 
 		if(alarm == nullptr)
+			sleep_time = 1;
+		else
+			{
+				alarm_list = alarm->link;
+				now = time(nullptr);
+				if(alarm->time < = now)
+					sleep_time = 0;
+				else
+					sleep_time = alarm->time - now;
+#ifdef DEBUG
+		printf("[waiting: %d(%d)\"%s\"]\n"), alarm_time, sleep_time, alarm->message);
+#endif
+		}
+		status = pthread_mutex_unlock(&alarm_mutex);
+		if(status != 0)
+			err_abort(status, "Error while unlocking mutex");
+		if(sleep_time > 0)
+			sleep(sleep_time);
+		else
+			sched_yield();
+
+		if(alarm != nullptr) {
+			printf("(%d) %s\n", alarm->seconds, alarm->message);
+			free(alarm);
+		}
 	}
 
 }
