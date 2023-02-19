796cb73b23d2226ad41fc60be841f2cffb09ba20
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-02-19 15:37:31 +0100

    First backoff implementation finished, but not working yet.

--- posix_threads/mutex/backoff_algorithm.cpp
+++ posix_threads/mutex/backoff_algorithm.cpp
@@ -31,16 +31,98 @@ void *lock_forward(void *arguments)
 				if (status == EBUSY) {
 					backoffs++;
 					DPRINTF((" [forward locker backing off at mutex %d ]\n", i));
-					for(; i>=0; i--)
-					{
+					for (; i >= 0; i--) {
 						status = pthread_mutex_unlock(&mutex_array[i]);
 						if (status != 0)
-							err_abort(status, "Error during BACKOFF!")
+							err_abort(status, "Error during BACKOFF!");
 					}
 				}
-
+				else {
+					if (status != 0)
+						err_abort(status, "Error while locking mutex");
+					DPRINTF ((" forward locker got mutex %d locked\n", i));
+				}
+			}
+			if (yield_flag) {
+				if (yield_flag > 0)
+					sched_yield();
+				else
+					sleep(1);
 			}
 
 		}
+		printf("lock_forward got all locks, %d backoffs \n", backoffs);
+		pthread_mutex_unlock(&mutex_array[2]);
+		pthread_mutex_unlock(&mutex_array[1]);
+		pthread_mutex_unlock(&mutex_array[0]);
+		sched_yield();
+
 	}
+	return nullptr;
+}
+
+void *lock_backward(void *arguments)
+{
+	int i{}, iterate{}, backoffs{};
+	int status{};
+	for (iterate = 0; iterate < ITERATIONS; ++iterate) {
+		backoffs = 0;
+		for (i = 2; i > -1; --i) {
+			if (i == 2)
+				status = pthread_mutex_lock(&mutex_array[i]);
+			if (status != 0)
+				err_abort(status, "Error in lock_forward, locking first mutex");
+			else {
+				if (backoff)
+					status = pthread_mutex_trylock(&mutex_array[i]);
+				else
+					status = pthread_mutex_lock(&mutex_array[i]);
+				if (status == EBUSY) {
+					backoffs++;
+					DPRINTF((" [forward locker backing off at mutex %d ]\n", i));
+					for (; i < 3; i++) {
+						status = pthread_mutex_unlock(&mutex_array[i]);
+						if (status != 0)
+							err_abort(status, "Error during BACKOFF!");
+					}
+				}
+				else {
+					if (status != 0)
+						err_abort(status, "Error while locking mutex");
+					DPRINTF ((" forward locker got mutex %d locked\n", i));
+				}
+			}
+			if (yield_flag) {
+				if (yield_flag > 0)
+					sched_yield();
+				else
+					sleep(1);
+			}
+
+		}
+		printf("lock_backward got all locks, %d backoffs \n", backoffs);
+		pthread_mutex_unlock(&mutex_array[0]);
+		pthread_mutex_unlock(&mutex_array[1]);
+		pthread_mutex_unlock(&mutex_array[2]);
+		sched_yield();
+	}
+	return nullptr;
+}
+
+int main(int number_of_arguments, char *arguments[])
+{
+	pthread_t forward_thread{}, backward_thread{};
+	int status{};
+	if (number_of_arguments > 1)
+		backoff = atoi(arguments[1]);
+
+	if (number_of_arguments > 2)
+		yield_flag = atoi(arguments[2]);
+	status = pthread_create(&forward_thread, nullptr, lock_forward, nullptr);
+	if (status != 0)
+		err_abort(status, "Error while crating forward thread!");
+	status = pthread_create(&backward_thread, nullptr, lock_backward, nullptr);
+	if (status != 0)
+		err_abort(status, "Error while crating backward thread!");
+	pthread_exit(nullptr);
 }
