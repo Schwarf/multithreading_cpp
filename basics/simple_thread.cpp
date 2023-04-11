f1352880631ab3591d0eebb9b599127a83d23719
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-11 15:30:28 +0200

    Async does not help on small time scales.

--- basics/simple_thread.cpp
+++ basics/simple_thread.cpp
@@ -3,16 +3,48 @@
 //
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <future>
+#include <random>
+#include "./../measure/time_measurement.h"
 
-void hello()
+template <typename T>
+T sum(const std::vector<T> & input)
 {
-	std::cout << "Hello thread" << std::endl;
+	return std::accumulate(input.begin(), input.end(), T{});
 }
+
+template <typename T>
+void sort(std::vector<T> & input)
+{
+	std::sort(input.begin(), input.end());
+}
+
 
 int main()
 {
-	std::thread t(hello);
-	t.join();
-	auto number_of_threads = std::thread::hardware_concurrency();
-	std::cout <<"Number of concurrent threads supported by implementation (is only a hint): " << number_of_threads << std::endl;
+	long long min{-31313190};
+	long long  max{2152921};
+	std::vector<long long> simple_input(std::ranges::views::iota(min, max).begin(), std::ranges::views::iota(min, max).end());
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::shuffle(simple_input.begin(), simple_input.end(), g);
+
+	auto time0 = std::chrono::steady_clock::now();
+	sort(simple_input);
+	sum(simple_input);
+	//auto sort_result = std::async(std::launch::async, sort<long long>, std::ref(simple_input));
+	//auto sum_result = std::async(std::launch::async, sum<long long>, std::ref(simple_input));
+	//sort_result.wait();
+	//sum_result.wait();
+
+	//std::cout << sum_result.get() << std::endl;
+	//sort_result.get();
+	auto time1 = std::chrono::steady_clock::now();
+	std::chrono::nanoseconds duration = time1-time0;
+	std::chrono::duration<double, std::milli> d(duration);
+	std::cout << d.count() << std::endl;
+
 }
