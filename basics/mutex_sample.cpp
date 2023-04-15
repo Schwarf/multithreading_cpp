0278774eaa5bb0eb1b26333302dcf3ea08089066
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-15 18:13:27 +0200

    Add central CMakeList. Work out one simple mutex example.

--- basics/mutex_sample.cpp
+++ basics/mutex_sample.cpp
@@ -3,42 +3,48 @@
 //
 #include <iostream>
 #include <mutex>
+#include <future>
+#include <list>
 #include <thread>
 
-using namespace std;
+std::list<int> list;
+std::mutex mutex;
 
-mutex myMutex;
-
-void shared_cout(int i)
+void add_to_list(int new_value)
 {
-	lock_guard<mutex> g(myMutex);
-	cout << " " << i << " ";
+	std::lock_guard<std::mutex> lock_guard(mutex);
+	list.push_back(new_value);
 }
 
-void f(int n)
+bool does_list_contain(int value_to_find)
 {
-	for(int i = 10*(n-1); i < 10*n ; i++) {
-		shared_cout(i);
-	}
+	std::lock_guard<std::mutex> locak_guard(mutex);
+	return std::find(list.begin(), list.end(), value_to_find) != list.end();
+}
+
+void print_list()
+{
+	for (const auto & it: list)
+		std::cout << it<< std::endl;
+
 }
 
 int main()
 {
-	thread t1(f, 1);  // 0-9
-	thread t2(f, 2);  // 10-19
-	thread t3(f, 3);  // 20-29
-	thread t4(f, 4);  // 30-39
-	thread t5(f, 5);  // 40-49
+	std::packaged_task<bool(int)> task(does_list_contain);
+	std::future<bool> f = task.get_future();
 
-
-	for(int i = 0; i > -50; i--)
-		shared_cout(i);  // (0, -49)
+	std::thread t1(add_to_list, 1);  // 0-9
+	std::thread t2(add_to_list, 2);  // 0-9
+	std::thread t3(add_to_list, 3);  // 0-9
+	std::thread t4(std::move(task), 1);  // 10-19
 
 	t1.join();
 	t2.join();
 	t3.join();
 	t4.join();
-	t5.join();
-
+	print_list();
+	std::cout << "Result : " << f.get() << std::endl;
 	return 0;
 }
+
