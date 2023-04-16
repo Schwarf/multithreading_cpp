38668cda70ffe0b35c396c7c57be0d51212fdfae
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-16 17:42:51 +0200

    Run second test 100 times.

--- basics/test/test_thread_safe_stack.cpp
+++ basics/test/test_thread_safe_stack.cpp
@@ -36,41 +36,45 @@ TEST(SetupThreadSafeStackTest, test_push_and_pop)
 
 TEST(ThreadSafeStackTest, test_push_and_pop_check_values)
 {
-	constexpr int number_of_threads{2};
-	constexpr int number_of_operations{1000};
-	ThreadSafeStack<int> stack;
-	std::vector<int> input(number_of_operations);
-	std::iota(input.begin(), input.end(), 0);
-	std::vector<int> result_values;
-	std::vector<int> expected_values;
-	std::vector<std::thread> threads;
-	std::mutex result_mutex;
-	auto fill_stack_per_thread =
-		[&stack, &result_values, &result_mutex, number_of_operations]()
-		{
-			for (int i{}; i < number_of_operations; ++i) {
-				stack.push(i);
-				int value;
-				bool popped = stack.pop(value);
-				ASSERT_TRUE(popped);
-				std::lock_guard<std::mutex> lock(result_mutex);
-				result_values.push_back(value);
-			}
-		};
-	for (int i = 0; i < number_of_threads; ++i) {
-		threads.emplace_back(fill_stack_per_thread);
-		expected_values.insert(expected_values.end(), input.begin(), input.end());
+	int run_test{100};
+	for (int run{}; run < run_test; run++) {
+
+		constexpr int number_of_threads{10};
+		constexpr int number_of_operations{1000};
+		ThreadSafeStack<int> stack;
+		std::vector<int> input(number_of_operations);
+		std::iota(input.begin(), input.end(), 0);
+		std::vector<int> result_values;
+		std::vector<int> expected_values;
+		std::vector<std::thread> threads;
+		std::mutex result_mutex;
+		auto fill_stack_per_thread =
+			[&stack, &result_values, &result_mutex, number_of_operations]()
+			{
+				for (int i{}; i < number_of_operations; ++i) {
+					stack.push(i);
+					int value;
+					bool popped = stack.pop(value);
+					ASSERT_TRUE(popped);
+					std::lock_guard<std::mutex> lock(result_mutex);
+					result_values.push_back(value);
+				}
+			};
+		for (int i = 0; i < number_of_threads; ++i) {
+			threads.emplace_back(fill_stack_per_thread);
+			expected_values.insert(expected_values.end(), input.begin(), input.end());
+		}
+
+		// Wait for all threads to finish
+		for (auto &thread: threads) {
+			thread.join();
+		}
+
+		// Check that all result_values were pushed and popped
+
+		ASSERT_NE(result_values, expected_values);
+		std::sort(result_values.begin(), result_values.end());
+		std::sort(expected_values.begin(), expected_values.end());
+		ASSERT_EQ(result_values, expected_values);
 	}
-
-	// Wait for all threads to finish
-	for (auto &thread: threads) {
-		thread.join();
-	}
-
-	// Check that all result_values were pushed and popped
-
-	ASSERT_NE(result_values, expected_values);
-	std::sort(result_values.begin(), result_values.end());
-	std::sort(expected_values.begin(), expected_values.end());
-	ASSERT_EQ(result_values, expected_values);
 }
