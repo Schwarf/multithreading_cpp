41475269a04ee561adfcae5f442031b623e70062
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-23 16:27:15 +0200

    Start implementation of deleting nodes for lock-free stack.

--- basics/lock_free_stack.h
+++ basics/lock_free_stack.h
@@ -21,6 +21,40 @@ private:
 		: value_(std::make_shared<T>(value)){}
 	};
 	std::atomic<Node*> head;
+	std::atomic<unsigned int> number_of_threads_in_pop{};
+	std::atomic<Node*> to_be_deleted;
+	static void delete_nodes(Node * node)
+	{
+		while(node)
+		{
+			Node * next = node->next;
+			delete node;
+			node = next;
+		}
+	}
+	void try_reclaim(Node * old_head)
+	{
+		if(number_of_threads_in_pop == 1)
+		{
+			Node* nodes_to_be_deleted = to_be_deleted.exchange(nullptr);
+			if(number_of_threads_in_pop == 1)
+			{
+				delete_nodes(to_be_deleted);
+			}
+			else if(nodes_to_be_deleted)
+			{
+				chain_pending_nodes(nodes_to_be_deleted);
+			}
+			delete old_head;
+		}
+		else
+		{
+			chain_pending_nodes(old_head);
+			--number_of_threads_in_pop;
+		}
+	}
+	
+
 public:
 	void push(T const & new_value)
 	{
@@ -32,8 +66,14 @@ public:
 
 	std::shared_ptr<T> popo()
 	{
+		++number_of_threads_in_pop;
 		Node* current_head = head.load();
 		while(current_head &&  !head.compare_exchange_weak(current_head, current_head->next));
+		std::shared_ptr<T> result;
+		if(current_head)
+		{
+			result.swap(current_head->value);
+		}
 		return current_head ? current_head->value_ : nullptr;
 	}
 
