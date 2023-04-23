6c642f6f9dc1499d6187489db6f6435d76d883b7
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-23 15:20:45 +0200

    Start with lock-free stack.

---
+++ basics/lock_free_stack.h
@@ -0,0 +1,31 @@
+//
+// Created by andreas on 23.04.23.
+//
+
+#ifndef LOCK_FREE_STACK_H
+#define LOCK_FREE_STACK_H
+#include <atomic>
+
+template <typename T>
+class LockFreeStack
+{
+private:
+	struct Node
+	{
+		T value_;
+		Node * next;
+		Node(T const & value)
+		: value_(value){}
+	};
+	std::atomic<Node*> head;
+public:
+	void push(T const & new_value)
+	{
+		Node * const new_node = new Node(new_value);
+		new_node->next = head->load();
+		// Typically use 'compare_exchange_weak' in a loop because in can fail spuriously
+		while(!head.compare_exchange_weak(new_node->next, new_node));
+	}
+
+};
+#endif //LOCK_FREE_STACK_H
