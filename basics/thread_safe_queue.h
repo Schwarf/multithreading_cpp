442f4acfc66a24ca68de19f5c49021140a908900
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-01 13:13:48 +0200

    Threadsafe queue.

--- basics/thread_safe_queue.h
+++ basics/thread_safe_queue.h
@@ -14,7 +14,11 @@ class ThreadSafeQueue
 
 public:
 	ThreadSafeQueue() = default;
-	ThreadSafeQueue(const ThreadSafeQueue<T> &other);
+	ThreadSafeQueue(const ThreadSafeQueue<T> &other)
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		data_ = other.data_;
+	}
 	ThreadSafeQueue &operator=(const ThreadSafeQueue<T> &other) = delete;
 	void push(T value)
 	{
@@ -30,13 +34,44 @@ public:
 		value = data_.front();
 		data_.pop();
 	}
-	bool try_pop(T &value);
-	std::shared_ptr<T> try_pop();
-	std::shared_ptr<T> wait_and_pop();
-	bool empty() const;
+	bool try_pop(T &value)
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (data_.empty())
+			return false;
+		value = data_.front();
+		data_.pop();
+		return true;
+	}
+
+	std::shared_ptr<T> try_pop()
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		if (data_.empty())
+			return std::shared_ptr<T>();
+		std::shared_ptr<T> result(std::make_shared<T>(data_.front()));
+		data_.pop();
+		return result;
+	}
+
+	std::shared_ptr<T> wait_and_pop()
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+		condition_.wait(lock, [this]
+		{ return !data_.empty(); });
+		std::shared_ptr<T> res(std::make_shared<T>(data_.front()));
+		data_.pop();
+		return res;
+	}
+
+	bool empty() const
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		return data_.empty();
+	}
 
 private:
-	std::mutex mutex_;
+	mutable std::mutex mutex_;
 	std::queue<T> data_;
 	std::condition_variable condition_;
 
