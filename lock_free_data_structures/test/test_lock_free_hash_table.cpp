e6f21b816bcc97bd1f3749d102c234eacf93d3bf
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-04-26 12:08:02 +0200

    Add first multithreaded test.

--- lock_free_data_structures/test/test_lock_free_hash_table.cpp
+++ lock_free_data_structures/test/test_lock_free_hash_table.cpp
@@ -1,6 +1,8 @@
 //
 // Created by andreas on 26.04.25.
 //
+#include <thread>
+
 #include "./../lock_free_hash_table.h"
 #include "gtest/gtest.h"
 
@@ -42,3 +44,35 @@ TEST(LockFreeHashTableTest, FullTableThrowsException) {
     EXPECT_FALSE(table.insert(2048, 9999));
 }
 
+TEST(LockFreeHashTableTest, MultiThreadedInsertions) {
+    // repeat 1000 times
+    for (int repeat = 0; repeat < 1000; ++repeat)
+    {
+        HashTable table;
+        constexpr int num_threads = 8;
+        constexpr int inserts_per_thread = 64;
+
+        std::vector<std::thread> threads;
+        for (int thread = 0; thread < num_threads; ++thread) {
+            threads.emplace_back([&table, thread]() {
+                for (int i = 0; i < inserts_per_thread; ++i) {
+                    int key = thread * inserts_per_thread + i;
+                    int value = key * 10;
+                    table.insert(key, value);
+                }
+            });
+        }
+
+        for (auto& th : threads) {
+            th.join();
+        }
+
+        // Now verify all insertions
+        for (int thread = 0; thread < num_threads; ++thread) {
+            for (int i = 0; i < inserts_per_thread; ++i) {
+                int key = thread * inserts_per_thread + i;
+                EXPECT_EQ(table.lookup(key), key * 10);
+            }
+        }
+    }
+}
