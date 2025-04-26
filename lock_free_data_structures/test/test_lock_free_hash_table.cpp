7217e87f1e62c8a37a49fe20da01824df345492a
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-04-26 15:41:35 +0200

    Test chaotic insertion.

--- lock_free_data_structures/test/test_lock_free_hash_table.cpp
+++ lock_free_data_structures/test/test_lock_free_hash_table.cpp
@@ -1,6 +1,7 @@
 //
 // Created by andreas on 26.04.25.
 //
+#include <random>
 #include <thread>
 
 #include "./../lock_free_hash_table.h"
@@ -70,9 +71,9 @@ TEST(LockFreeHashTableTest, MultiThreadedInsertions)
             });
         }
 
-        for (auto& th : threads)
+        for (auto& thread : threads)
         {
-            th.join();
+            thread.join();
         }
 
         // Now verify all insertions
@@ -81,6 +82,51 @@ TEST(LockFreeHashTableTest, MultiThreadedInsertions)
             for (int i = 0; i < inserts_per_thread; ++i)
             {
                 int key = thread * inserts_per_thread + i;
+                EXPECT_EQ(table.lookup(key), key * 10);
+            }
+        }
+    }
+}
+
+TEST(LockFreeHashTableTest, ChaoticMultiThreadedInsertions) {
+    constexpr int num_threads = 8;
+    constexpr int inserts_per_thread = 128;
+
+    for (int repeat = 0; repeat < 100; ++repeat) {
+        HashTable table;
+        std::vector<std::vector<int>> thread_keys(num_threads);
+        std::vector<std::thread> threads;
+        for (int thread = 0; thread < num_threads; ++thread) {
+            threads.emplace_back([&table, thread, &thread_keys]() {
+                std::mt19937 rng(std::random_device{}());
+                std::uniform_int_distribution<int> sleep_chance(0, 9);
+
+                std::vector<int> keys;
+                for (int i = 0; i < inserts_per_thread; ++i) {
+                    keys.push_back(thread * inserts_per_thread + i);
+                }
+                std::shuffle(keys.begin(), keys.end(), rng);
+                thread_keys[thread] = keys;
+
+                for (int key : keys) {
+                    int value = key * 10;
+                    table.insert(key, value);
+
+                    if (sleep_chance(rng) == 0) {
+                        std::this_thread::yield();
+                        std::this_thread::sleep_for(std::chrono::microseconds(10));
+                    }
+                }
+            });
+        }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
+        // Verify all insertions
+        for (int thread = 0; thread < num_threads; ++thread) {
+            for (const auto key : thread_keys[thread]) {
                 EXPECT_EQ(table.lookup(key), key * 10);
             }
         }
