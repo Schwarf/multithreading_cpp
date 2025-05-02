23988f25f0d133fcfa6b7b08c4f94c6c9824ff82
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-05-02 13:14:53 +0200

    Add some hazard pointer functionality.

--- basics/lock_free_stack.h
+++ basics/lock_free_stack.h
@@ -13,8 +13,8 @@ constexpr unsigned int maximum_number_of_threads{24};
 
 struct HazardPointer
 {
-	std::atomic<std::thread::id> thread_id;
-	std::atomic<void*> pointer;
+    std::atomic<std::thread::id> thread_id;
+    std::atomic<void*> pointer;
 };
 
 inline HazardPointer hazard_pointers[maximum_number_of_threads];
@@ -22,15 +22,16 @@ inline HazardPointer hazard_pointers[maximum_number_of_threads];
 class HazardPointerOwner
 {
 private:
-	HazardPointer * hazard_pointer;
-public:
-	HazardPointerOwner(const HazardPointer & rhs) = delete;
-	HazardPointerOwner operator=(const HazardPointer & rhs) = delete;
-	HazardPointerOwner():
-	hazard_pointer(nullptr)
-	{
-	}
+    HazardPointer* hazard_pointer;
 
+public:
+    HazardPointerOwner(const HazardPointer& rhs) = delete;
+    HazardPointerOwner operator=(const HazardPointer& rhs) = delete;
+
+    HazardPointerOwner():
+        hazard_pointer(nullptr)
+    {
+    }
 };
 
 
@@ -38,96 +39,132 @@ template <typename T>
 class LockFreeStack
 {
 private:
-	struct Node
-	{
+    struct Node
+    {
+        std::shared_ptr<T> value_;
+        Node* next;
 
-		std::shared_ptr<T> value_;
-		Node * next;
-		explicit Node(T const & value)
-		// We allocate the data on the heap in the Node
-		: value_(std::make_shared<T>(value)),
-		next(nullptr)
-		{}
-	};
-	std::atomic<Node *> head;
-	std::atomic<unsigned int> number_of_threads_in_pop{};
-	std::atomic<Node*> to_be_deleted;
-	static void delete_nodes(Node * node)
-	{
-		while(node)
-		{
-			Node * next = node->next;
-			delete node;
-			node = next;
-		}
-	}
+        explicit Node(T const& value)
+        // We allocate the data on the heap in the Node
+            : value_(std::make_shared<T>(value)),
+              next(nullptr)
+        {
+        }
+    };
 
-	void chain_nodes_for_deletion(Node * node)
-	{
-		Node * last = node;
-		while(Node* const next = last->next)
-		{
-			last = next;
-		}
-		chain_nodes_for_deletion(node, last);
-	}
+    std::atomic<Node*> head;
+    std::atomic<unsigned int> number_of_threads_in_pop{};
+    std::atomic<Node*> to_be_deleted;
 
-	void chain_nodes_for_deletion(Node * first, Node* last)
-	{
-		last->next = to_be_deleted;
-		while(!to_be_deleted.compare_exchange_weak(last->next, first));
-	}
+    static void delete_nodes(Node* node)
+    {
+        while (node)
+        {
+            Node* next = node->next;
+            delete node;
+            node = next;
+        }
+    }
 
-	void chain_single_node_for_deletion(Node* node)
-	{
-		chain_nodes_for_deletion(node, node);
-	}
+    void chain_nodes_for_deletion(Node* node)
+    {
+        Node* last = node;
+        while (Node* const next = last->next)
+        {
+            last = next;
+        }
+        chain_nodes_for_deletion(node, last);
+    }
 
-	void try_reclaim_memory(Node * old_head)
-	{
-		if(number_of_threads_in_pop == 1)
-		{
-			Node* nodes_to_be_deleted = to_be_deleted.exchange(nullptr);
-			if(number_of_threads_in_pop == 1)
-			{
-				delete_nodes(nodes_to_be_deleted);
-			}
-			else if(nodes_to_be_deleted)
-			{
-				chain_nodes_for_deletion(nodes_to_be_deleted);
-			}
-			delete old_head;
-		}
-		else
-		{
-			chain_single_node_for_deletion(old_head);
-			--number_of_threads_in_pop;
-		}
-	}
-	
+    void chain_nodes_for_deletion(Node* first, Node* last)
+    {
+        last->next = to_be_deleted;
+        while (!to_be_deleted.compare_exchange_weak(last->next, first));
+    }
+
+    void chain_single_node_for_deletion(Node* node)
+    {
+        chain_nodes_for_deletion(node, node);
+    }
+
+    void try_reclaim_memory(Node* old_head)
+    {
+        if (number_of_threads_in_pop == 1)
+        {
+            Node* nodes_to_be_deleted = to_be_deleted.exchange(nullptr);
+            if (number_of_threads_in_pop == 1)
+            {
+                delete_nodes(nodes_to_be_deleted);
+            }
+            else if (nodes_to_be_deleted)
+            {
+                chain_nodes_for_deletion(nodes_to_be_deleted);
+            }
+            delete old_head;
+        }
+        else
+        {
+            chain_single_node_for_deletion(old_head);
+            --number_of_threads_in_pop;
+        }
+    }
 
 public:
-	void push(const T & new_value)
-	{
-		Node * const new_node = new Node(new_value);
-		new_node->next = head.load();
-		// Typically use 'compare_exchange_weak' in a loop because in can fail spuriously
-		while(!head.compare_exchange_weak(new_node->next, new_node));
-	}
+    void push(const T& new_value)
+    {
+        Node* const new_node = new Node(new_value);
+        new_node->next = head.load();
+        // Typically use 'compare_exchange_weak' in a loop because in can fail spuriously
+        while (!head.compare_exchange_weak(new_node->next, new_node));
+    }
 
-	std::shared_ptr<T> pop()
-	{
-		++number_of_threads_in_pop;
-		Node* old_head = head.load();
-		while(old_head &&  !head.compare_exchange_weak(old_head, old_head->next));
-		std::shared_ptr<T> result;
-		if(old_head)
-		{
-			result.swap(old_head->value_);
-		}
-		try_reclaim_memory(old_head);
-		return result;
-	}
+    std::shared_ptr<T> pop()
+    {
+        std::atomic<void*>& hp = get_hazard_pointer_for_current_thread();
+        Node* old_head = head.load();
+        do
+        {
+            Node* temp;
+            do
+            {
+                temp = old_head;
+                hp.store(old_head);
+                old_head = head.load();
+            }
+            while (old_head != temp);
+        }
+        while (old_head &&
+            !head.compare_exchange_strong(old_head, old_head->next));
+        hp.store(nullptr);
+        std::shared_ptr<T> result;
+        if (old_head)
+        {
+            result.swap(old_head->data);
+            if (outstanding_hazard_pointers_for(old_head))
+            {
+                reclaim_later(old_head);
+            }
+            else
+            {
+                delete old_head;
+            }
+            delete_nodes_with_no_hazards();
+        }
+        return result;
+    }
 
+    // std::shared_ptr<T> pop()
+    // {
+    // 	++number_of_threads_in_pop;
+    // 	Node* old_head = head.load();
+    // 	while(old_head &&  !head.compare_exchange_weak(old_head, old_head->next));
+    // 	std::shared_ptr<T> result;
+    // 	if(old_head)
+    // 	{
+    // 		result.swap(old_head->value_);
+    // 	}
+    // 	try_reclaim_memory(old_head);
+    // 	return result;
+    // }
 };
 #endif //LOCK_FREE_STACK_H
