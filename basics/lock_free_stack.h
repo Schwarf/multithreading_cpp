4ab413ae85ec18e9554e99fc08e126f5a40359e8
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-05-02 13:32:03 +0200

    Add hazard pointer from C++ Concurrency in Action.

--- basics/lock_free_stack.h
+++ basics/lock_free_stack.h
@@ -9,30 +9,56 @@
 #include <stack>
 #include <thread>
 
-constexpr unsigned int maximum_number_of_threads{24};
-
-struct HazardPointer
+unsigned const max_hazard_pointers=100;
+struct hazard_pointer
 {
-    std::atomic<std::thread::id> thread_id;
+    std::atomic<std::thread::id> id;
     std::atomic<void*> pointer;
 };
 
-inline HazardPointer hazard_pointers[maximum_number_of_threads];
+hazard_pointer hazard_pointers[max_hazard_pointers];
 
-class HazardPointerOwner
+class hp_owner
 {
-private:
-    HazardPointer* hazard_pointer;
-
+    hazard_pointer* hp;
 public:
-    HazardPointerOwner(const HazardPointer& rhs) = delete;
-    HazardPointerOwner operator=(const HazardPointer& rhs) = delete;
-
-    HazardPointerOwner():
-        hazard_pointer(nullptr)
+    hp_owner(hp_owner const&)=delete;
+    hp_owner operator=(hp_owner const&)=delete;
+    hp_owner():
+    hp(nullptr)
     {
+        for(unsigned i=0;i<max_hazard_pointers;++i)
+        {
+            std::thread::id old_id;
+            if(hazard_pointers[i].id.compare_exchange_strong(
+            old_id,std::this_thread::get_id()))
+            {
+                hp=&hazard_pointers[i];
+                break;
+            }
+        }
+        if(!hp)
+        {
+            throw std::runtime_error("No hazard pointers available");
+        }
+    }
+    std::atomic<void*>& get_pointer()
+    {
+        return hp->pointer;
+    }
+    ~hp_owner()
+    {
+        hp->pointer.store(nullptr);
+        hp->id.store(std::thread::id());
     }
 };
+
+std::atomic<void*>& get_hazard_pointer_for_current_thread()
+{
+    thread_local static hp_owner hazard;
+    return hazard.get_pointer();
+}
+
 
 
 template <typename T>
