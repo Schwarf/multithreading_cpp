f248dc4a1b50ae40f0e7e3a5c82a7a8c01a62327
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-05-02 13:59:32 +0200

    Add rest of functions for Hazard pointers.

--- basics/lock_free_stack.h
+++ basics/lock_free_stack.h
@@ -64,7 +64,7 @@ inline std::atomic<void*>& get_hazard_pointer_for_current_thread()
 
 inline bool outstanding_hazard_pointers_for(void* pointer)
 {
-    for (auto & hazard_pointer : hazard_pointers)
+    for (auto& hazard_pointer : hazard_pointers)
     {
         if (hazard_pointer.pointer.load() == pointer)
         {
@@ -72,6 +72,65 @@ inline bool outstanding_hazard_pointers_for(void* pointer)
         }
     }
     return false;
+}
+
+template <typename T>
+void do_delete(void* pointer)
+{
+    delete static_cast<T*>(pointer);
+}
+
+struct data_to_reclaim
+{
+    void* data;
+    std::function<void(void*)> deleter;
+    data_to_reclaim* next;
+
+    template <typename T>
+    explicit data_to_reclaim(T* pointer):
+        data(pointer),
+        deleter(&do_delete<T>),
+        next(0)
+    {
+    }
+
+    ~data_to_reclaim()
+    {
+        deleter(data);
+    }
+};
+
+std::atomic<data_to_reclaim*> nodes_to_reclaim;
+
+
+inline void add_to_reclaim_list(data_to_reclaim* node)
+{
+    node->next = nodes_to_reclaim.load();
+    while (!nodes_to_reclaim.compare_exchange_weak(node->next, node));
+}
+
+template <typename T>
+void reclaim_later(T* data)
+{
+    add_to_reclaim_list(new data_to_reclaim(data));
+}
+
+inline void delete_nodes_with_no_hazards()
+{
+    data_to_reclaim* current = nodes_to_reclaim.exchange(nullptr);
+    while (current)
+    {
+        data_to_reclaim* const next = current->next;
+        if (!outstanding_hazard_pointers_for(current->data))
+        {
+            delete current;
+        }
+        else
+        {
+            add_to_reclaim_list(current);
+        }
+        current = next;
+    }
 }
 
 template <typename T>
