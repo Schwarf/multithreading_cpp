55fe4f18d15d9a37924ca9d9821522c9114c0e24
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-05-04 14:32:48 +0200

    Add implementation without shared_ptr.

---
+++ basics/lock_free_stack.h
@@ -0,0 +1,186 @@
+//
+// Created by andreas on 04.05.25.
+//
+
+#ifndef LOCK_FREE_STACK_H
+#define LOCK_FREE_STACK_H
+#include <atomic>
+#include <cstddef>
+#include <stdexcept>
+#include <thread>
+
+template <typename T>
+concept NodeConcept = requires(T a)
+{
+    { T::data };
+    { *a.next } -> std::same_as<T&>;
+};
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node* next;
+
+    Node(T d): data(d), next(nullptr)
+    {
+    }
+};
+
+constexpr std::size_t MaxHazardPointers = 50;
+
+template <typename T, NodeConcept MyNode = Node<T>>
+struct HazardPointer
+{
+    std::atomic<std::thread::id> id;
+    std::atomic<MyNode*> pointer;
+};
+
+template <typename T>
+HazardPointer<T> HazardPointers[MaxHazardPointers];
+
+template <typename T, NodeConcept MyNode = Node<T>>
+class HazardPointerOwner
+{
+    HazardPointer<T>* hazardPointer;
+
+public:
+    HazardPointerOwner(HazardPointerOwner const&) = delete;
+    HazardPointerOwner operator=(HazardPointerOwner const&) = delete;
+
+    HazardPointerOwner() : hazardPointer(nullptr)
+    {
+        for (std::size_t i = 0; i < MaxHazardPointers; ++i)
+        {
+            std::thread::id old_id;
+            if (HazardPointers<T>[i].id.compare_exchange_strong(
+                old_id, std::this_thread::get_id()))
+            {
+                hazardPointer = &HazardPointers<T>[i];
+                break;
+            }
+        }
+        if (!hazardPointer)
+        {
+            throw std::out_of_range("No hazard pointers available!");
+        }
+    }
+
+    std::atomic<MyNode*>& getPointer()
+    {
+        return hazardPointer->pointer;
+    }
+
+    ~HazardPointerOwner()
+    {
+        hazardPointer->pointer.store(nullptr);
+        hazardPointer->id.store(std::thread::id());
+    }
+};
+
+template <typename T, NodeConcept Node = Node<T>>
+std::atomic<Node*>& getHazardPointer()
+{
+    thread_local static HazardPointerOwner<T> hazard;
+    return hazard.getPointer();
+}
+
+template <typename T, NodeConcept Node = Node<T>>
+class RetireList
+{
+    struct RetiredNode
+    {
+        Node* node;
+        RetiredNode* next;
+
+        RetiredNode(Node* p) : node(p), next(nullptr)
+        {
+        }
+
+        ~RetiredNode()
+        {
+            delete node;
+        }
+    };
+
+    std::atomic<RetiredNode*> RetiredNodes;
+
+    void addToRetiredNodes(RetiredNode* retiredNode)
+    {
+        retiredNode->next = RetiredNodes.load();
+        while (!RetiredNodes.compare_exchange_strong(retiredNode->next, retiredNode));
+    }
+
+public:
+    bool isInUse(Node* node)
+    {
+        for (std::size_t i = 0; i < MaxHazardPointers; ++i)
+        {
+            if (HazardPointers<T>[i].pointer.load() == node) return true;
+        }
+        return false;
+    }
+
+    void addNode(Node* node)
+    {
+        addToRetiredNodes(new RetiredNode(node));
+    }
+
+    void deleteUnusedNodes()
+    {
+        RetiredNode* current = RetiredNodes.exchange(nullptr);
+        while (current)
+        {
+            RetiredNode* const next = current->next;
+            if (!isInUse(current->node)) delete current;
+            else addToRetiredNodes(current);
+            current = next;
+        }
+    }
+};
+
+template <typename T, NodeConcept Node = Node<T>>
+class LockFreeStack
+{
+    std::atomic<Node*> head;
+    RetireList<T> retireList;
+
+public:
+    LockFreeStack() = default;
+    LockFreeStack(const LockFreeStack&) = delete;
+    LockFreeStack& operator=(const LockFreeStack&) = delete;
+
+    void push(T val)
+    {
+        Node* const newMyNode = new Node(val);
+        newMyNode->next = head.load();
+        while (!head.compare_exchange_strong(newMyNode->next, newMyNode));
+    }
+
+    T topAndPop()
+    {
+        std::atomic<Node*>& hazardPointer = getHazardPointer<T>();
+        Node* oldHead = head.load();
+        do
+        {
+            Node* tempMyNode;
+            do
+            {
+                tempMyNode = oldHead;
+                hazardPointer.store(oldHead);
+                oldHead = head.load();
+            }
+            while (oldHead != tempMyNode);
+        }
+        while (oldHead && !head.compare_exchange_strong(oldHead, oldHead->next)) ;
+        if (!oldHead) throw std::out_of_range("The stack is empty!");
+        hazardPointer.store(nullptr);
+        auto res = oldHead->data;
+        if (retireList.isInUse(oldHead)) retireList.addNode(oldHead);
+        else delete oldHead;
+        retireList.deleteUnusedNodes();
+        return res;
+    }
+};
+
+#endif //LOCK_FREE_STACK_H
---
+++ basics/test/test_lock_free_stack.cpp
@@ -0,0 +1,4 @@
+//
+// Created by andreas on 04.05.25.
+//
+#include "./../lock_free_stack.h"
